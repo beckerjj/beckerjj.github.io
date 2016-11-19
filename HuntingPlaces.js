39db5925049e698481f0b83d75921f375a4786b7
beckerjj <jbthatisme@hotmail.com>
2016-11-18 21:04:03 -0600

    Add files via upload

---
+++ HuntingPlaces.js
@@ -0,0 +1,58 @@
+var Places = [
+	{
+		name: "Hill Stool Lookout",
+		latitude: 44.2537167,
+		longitude: -90.7041167
+	},
+	{
+		name: "Hill Lookout",
+		latitude: 44.25278334,
+		longitude: -90.7061
+	},
+	{
+		name: "Bow spot 2015",
+		latitude: 44.2523334,
+		longitude: -90.7038167
+	},
+	{
+	    "name":"New tree",
+	    "longitude":-90.70098333333334,
+	    "latitude":44.25633333333333
+	},
+	{
+	    "name":"Hill sit",
+	    "longitude":-90.6999,
+	    "latitude":44.2583
+	},
+	{
+	    "name":"Monkey tree",
+	    "longitude":-90.70051666666667,
+	    "latitude":44.25703333333333
+	},
+	{
+	    "name":"East West Trail",
+	    "longitude":-90.70058333333333,
+	    "latitude":44.25951666666667
+	},
+	{
+	    "name":"Chain Tree",
+	    "longitude":-90,
+	    "longitudeMinutes":-41.987,
+	    "latitude":44,
+	    "latitudeMinutes":15.467
+	},
+	{
+	    "name":"Josh Tree",
+	    "longitude":-90,
+	    "longitudeMinutes":-44.825,
+	    "latitude":44,
+	    "latitudeMinutes":19.065
+	},
+	{
+	    "name":"Jeff Tree",
+	    "longitude":-90,
+	    "longitudeMinutes":-44.825,
+	    "latitude":44,
+	    "latitudeMinutes":19.173
+	}
+];
